26f5b71c19a4c400cf06cf77da4dbea920de07e6
sk3p7ic <joshuaibrom@gmail.com>
2022-11-02 22:23:21 -0500

    Started working on formatting the header

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,9 +9,10 @@ export default function RootLayout({
   return (
     <html>
       <head>
+        <meta name="viewport" content="width=device-width, intitial-scale=1" />
         <title>Joshua Ibrom</title>
       </head>
-      <body>
+      <body className="w-screen min-h-screen overflow-x-hidden">
         <Navbar />
         <div className="mx-2 sm:mx-4 md:mx-8 lg:mx-16 xl:mx-32">{children}</div>
       </body>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,23 @@
+import Link from "next/link";
+
 export const Navbar = () => {
   return (
-    <header>
-      <h2>Joshua Ibrom</h2>
+    <header className="w-screen flex flex-col md:flex-row md:justify-between px-2 sm:px-4 md:px-8 lg:px-16 xl:px-32 py-4">
+      <h2>joshua ibrom</h2>
+
+      <nav className="hidden md:flex flex-row gap-4 lg:gap-8">
+        <Link href="/#home">home</Link>
+        <Link href="/#projects">projects</Link>
+        <Link href="/#about">about</Link>
+        <Link href="/#contact">contact</Link>
+      </nav>
+
+      <nav className="flex md:hidden flex-col gap-2">
+        <Link href="/#home">home</Link>
+        <Link href="/#projects">projects</Link>
+        <Link href="/#about">about</Link>
+        <Link href="/#contact">contact</Link>
+      </nav>
     </header>
   );
 };
