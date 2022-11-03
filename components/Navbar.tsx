821d3eb09b1380e099b97b149c338de17b75bf93
Joshua Ibrom <joshuaibrom@gmail.com>
2022-11-02 22:34:09 -0500

    Created easier way of formatting nav links

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,22 +1,36 @@
 import Link from "next/link";
 
+type NavLink = {
+  href: string;
+  title: string;
+};
+
+const navLinks: NavLink[] = [
+  { href: "/#home", title: "home" },
+  { href: "/#projects", title: "projects" },
+  { href: "/#about", title: "about" },
+  { href: "/#contact", title: "contact" },
+];
+
 export const Navbar = () => {
   return (
     <header className="w-screen flex flex-col md:flex-row md:justify-between px-2 sm:px-4 md:px-8 lg:px-16 xl:px-32 py-4">
       <h2>joshua ibrom</h2>
 
       <nav className="hidden md:flex flex-row gap-4 lg:gap-8">
-        <Link href="/#home">home</Link>
-        <Link href="/#projects">projects</Link>
-        <Link href="/#about">about</Link>
-        <Link href="/#contact">contact</Link>
+        {navLinks.map((l, i) => (
+          <Link href={l.href} key={i}>
+            {l.title}
+          </Link>
+        ))}
       </nav>
 
       <nav className="flex md:hidden flex-col gap-2">
-        <Link href="/#home">home</Link>
-        <Link href="/#projects">projects</Link>
-        <Link href="/#about">about</Link>
-        <Link href="/#contact">contact</Link>
+        {navLinks.map((l, i) => (
+          <Link href={l.href} key={i}>
+            {l.title}
+          </Link>
+        ))}
       </nav>
     </header>
   );
