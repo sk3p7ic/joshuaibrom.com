5c1995d166e3c641c76224f85129d0a119ed15d7
Joshua Ibrom <joshuaibrom@gmail.com>
2022-11-04 09:42:39 -0500

    Started working on the projects page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { Banner } from "../components/Home/Banner";
+import { Projects } from "../components/Home/Projects";
 
 const HomePage = () => {
   return (
-    <main>
+    <main className="flex flex-col gap-8">
       <Banner />
+      <Projects />
     </main>
   );
 };
---
+++ components/Home/Projects.tsx
@@ -0,0 +1,9 @@
+export const Projects = () => {
+  return (
+    <div className="flex flex-row gap-4" id="projects">
+      <h3 className="font-raleway text-4xl text-emerald-700 dark:text-cyan-500 transition-colors duration-1000">
+        projects.
+      </h3>
+    </div>
+  );
+};
