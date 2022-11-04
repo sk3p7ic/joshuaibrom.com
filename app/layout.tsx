33d228b8cb9ea69d0dba98d37c0dc9f81c0026ff
Joshua Ibrom <joshuaibrom@gmail.com>
2022-11-03 21:52:35 -0500

    Changed mode transition dur to not be blinding

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, intitial-scale=1" />
         <title>Joshua Ibrom</title>
       </head>
-      <body className="w-screen min-h-screen overflow-x-hidden bg-stone-100 text-stone-800 dark:bg-stone-800 dark:text-stone-100 transition-colors">
+      <body className="w-screen min-h-screen overflow-x-hidden bg-stone-100 text-stone-800 dark:bg-stone-800 dark:text-stone-100 transition-colors duration-1000">
         <Navbar />
         <div className="mx-2 sm:mx-4 md:mx-8 lg:mx-16 xl:mx-32">{children}</div>
       </body>
