47e4960ce35a8fb1c26bd96d753f8909d3a96f87
sk3p7ic <joshuaibrom@gmail.com>
2022-11-05 14:31:22 -0500

    Reworked card style

--- components/Home/Project-Cards/ProjectCard.tsx
+++ components/Home/Project-Cards/ProjectCard.tsx
@@ -20,10 +20,12 @@ export const ProjectCard = ({ project, baseImagePath }: ProjectCardProps) => {
   const getImagePath = () => baseImagePath + project.headerImgSrc;
 
   return (
-    <div className="flex flex-col gap-2 bg-stone-300 dark:bg-stone-900 rounded-lg overflow-hidden">
+    <div className="flex flex-col gap-2 bg-amber-100 dark:bg-stone-900 rounded-lg overflow-hidden">
       <img src={getImagePath()} />
       <div className="px-2 flex flex-col md:flex-row justify-end md:items-end md:justify-between">
-        <h1 className="font-nunito text-2xl">{project.name.toLowerCase()}</h1>
+        <h1 className="font-nunito text-2xl font-bold">
+          {project.name.toLowerCase()}
+        </h1>
         <h2 className="font-nunito text-lg">{project.type}</h2>
       </div>
       <p className="px-4 pb-4 font-inter text-justify">{project.description}</p>
