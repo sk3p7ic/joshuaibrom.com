b2d05ac0f193a95204c5a5643f3deef27797d8d9
sk3p7ic <joshuaibrom@gmail.com>
2022-11-05 14:20:06 -0500

    Worked on project cards

---
+++ components/Home/Project-Cards/ProjectCard.tsx
@@ -0,0 +1,32 @@
+type ProjectType = {
+  name: string;
+  stack: string[][];
+  type: string;
+  headerImgSrc: string;
+  links: {
+    type: string;
+    url: string;
+    tooltip: string;
+  }[];
+  description: string;
+};
+
+export type ProjectCardProps = {
+  project: ProjectType;
+  baseImagePath: string;
+};
+
+export const ProjectCard = ({ project, baseImagePath }: ProjectCardProps) => {
+  const getImagePath = () => baseImagePath + project.headerImgSrc;
+
+  return (
+    <div className="flex flex-col gap-2 bg-stone-300 dark:bg-stone-900 rounded-lg overflow-hidden">
+      <img src={getImagePath()} />
+      <div className="px-2 flex flex-col md:flex-row justify-end md:items-end md:justify-between">
+        <h1 className="font-nunito text-2xl">{project.name.toLowerCase()}</h1>
+        <h2 className="font-nunito text-lg">{project.type}</h2>
+      </div>
+      <p className="px-4 pb-4 font-inter text-justify">{project.description}</p>
+    </div>
+  );
+};
--- components/Home/Projects.tsx
+++ components/Home/Projects.tsx
@@ -1,9 +1,21 @@
+import ProjectData from "../../lib/projects.json";
+import { ProjectCard } from "./Project-Cards/ProjectCard";
+
 export const Projects = () => {
   return (
-    <div className="flex flex-row gap-4" id="projects">
+    <div className="flex flex-col gap-4" id="projects">
       <h3 className="font-raleway text-4xl text-emerald-700 dark:text-cyan-500 transition-colors duration-1000">
         projects.
       </h3>
+      <div className="grid grid-cols-3 gap-4">
+        {ProjectData.projects.map((p, i) => (
+          <ProjectCard
+            project={p}
+            baseImagePath={ProjectData.baseImgPath}
+            key={i}
+          />
+        ))}
+      </div>
     </div>
   );
 };
