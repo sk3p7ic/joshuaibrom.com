c31c04d9f31b95cec506c802c713963d8e835493
Joshua Ibrom <joshuaibrom@gmail.com>
2022-11-05 21:20:12 -0500

    Changed grid for project cards

--- components/Home/Projects.tsx
+++ components/Home/Projects.tsx
@@ -7,7 +7,7 @@ export const Projects = () => {
       <h3 className="font-raleway text-4xl text-emerald-700 dark:text-cyan-500 transition-colors duration-1000">
         projects.
       </h3>
-      <div className="grid grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
         {ProjectData.projects.map((p, i) => (
           <ProjectCard
             project={p}
